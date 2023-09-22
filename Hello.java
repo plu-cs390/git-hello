209ec63b2b8de747c68210908c5facd9c415268c
DW <daw42@users.noreply.github.com>
2023-09-22 14:42:58 -0700

    Add sayName

--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello Git");
+    sayName();
     System.out.println("Goodbye");
+  }
+
+  public static void sayName() {
+    System.out.println("Dave");
   }
 }
